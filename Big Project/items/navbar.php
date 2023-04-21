9c61dc0ff3288c4956543fa202088acd1b09448d
Arna2Guardia <117608420+Arna2Guardia@users.noreply.github.com>
2023-04-21 19:22:47 +0200

    Add files via upload

---
+++ Big Project/items/navbar.php
@@ -0,0 +1,28 @@
+<div class="navbar">
+        <div class="navbar-content">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Projets</a></li>
+                <li><a href="#">CV</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Jeu 1</a></li>
+                <li><a href="#">Jeu 2</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="navbar-tel">
+        <div class="navbar-content-tel">
+            <span id="navbar-logo-tel">logo</span>
+            <div id="nav-dropdown" class="navbar-links-tel">
+                <a href="index.php">Accueil</a>
+                <a href="projects.php">Projets</a>
+                <a href="cv.php">CV</a>
+                <a href="index.php#ellivro">Contact</a>
+                <a href="#">Jeu 1</a>
+                <a href="#">Jeu 2</a>
+            </div>
+            <a class="dropbtn" href="javascript:void(0);" onclick="dropnav()">
+                <span class="material-symbols-outlined">menu</span>
+            </a>
+        </div>
+    </div>
